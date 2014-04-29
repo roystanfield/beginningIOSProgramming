08a2736ce14138f773e21baf3500705b79abb679
Roy Stanfield <roystanfield@gmail.com>
2014-04-29 13:01:45 -0400

    Project for screen color blog post.

---
+++ ScreenColor/ScreenColor/JRSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JRSAppDelegate.h
+//  ScreenColor
+//
+//  Created by Roy Stanfield on 4/29/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JRSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScreenColor/ScreenColor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScreenColor
+//
+//  Created by Roy Stanfield on 4/29/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JRSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JRSAppDelegate class]));
+    }
+}
